12d3980b012526bd1c08495295fe000f1ab074e4
Dilip Yadav <mryadavdilip@gmail.com>
2025-05-24 17:41:49 +0530

    provider

--- lib/providers/chess_board_provider.dart
+++ lib/providers/chess_board_provider.dart
@@ -1,6 +1,15 @@
+import 'package:chess_interface/chess_interface_dart.dart';
 import 'package:flutter/material.dart';
 
 class ChessBoardProvider extends ChangeNotifier {
+  ChessBoardInterface _game = ChessBoardInterface();
+  ChessBoardInterface get game => _game;
+
+  void init(ChessBoardInterface newGame) {
+    _game = newGame;
+    notifyListeners();
+  }
+
   notify() {
     notifyListeners();
   }
